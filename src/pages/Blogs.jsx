b0d025d7aa2e2f1c23e17011b13bdd8d6c96b79f
Pradeep <Pradeepverma249403@gmail.com>
2026-05-03 16:35:10 +0530

    Added pdf loader

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -2,13 +2,28 @@ import React, { useEffect, useState } from "react";
 
 export default function Blogs() {
   const [magazines, setMagazines] = useState([]);
+  const [loadingMap, setLoadingMap] = useState({});
 
   useEffect(() => {
   fetch(`${import.meta.env.VITE_API_URL}/api/magazine`)
     .then((res) => res.json())
-    .then((data) => setMagazines(data));
-}, []);
+    .then((data) => {
+      setMagazines(data);
 
+      // initial loading true
+      const initialLoadingMap = {};
+      data.forEach((item) => {
+        initialLoadingMap[item._id] = true;
+      });
+      setLoadingMap(initialLoadingMap);
+    });
+
+    
+    
+}, [])
+const handleLoad = (id) => {
+      setLoadingMap((prev) => ({ ...prev, [id]: false }));
+};
   return (
     <section id="blogs" className="py-16 bg-gray-50 font-[Poppins]">
       <div className="max-w-7xl mx-auto px-4">
@@ -28,14 +43,26 @@ export default function Blogs() {
               <h3 className="text-xl font-semibold text-green-700 mb-3">
                 {mag.title}
               </h3>
+              {/* loader */}
+              <div className="relative w-full h-96">
+                {loadingMap[mag._id] && (
+                  <div className="absolute inset-0 flex items-center justify-center bg-gray-100 rounded">
+                    <div className="animate-spin rounded-full h-10 w-10 border-4 border-green-500 border-t-transparent">
 
-              {/* Buttons */}
-              <div className="flex flex-col gap-2 mt-2">
+                    </div>
+                  </div>
+                )}
                 <iframe
                   src={mag.pdfUrl}
                   className="w-full h-96 rounded mb-4"
                   title="PDF Preview"
+                  onLoad={() => handleLoad(mag._id)}
                 ></iframe>
+              </div>
+
+              {/* Buttons */}
+              <div className="flex flex-col gap-2 mt-2">
+                
                 <button
                   onClick={() =>
                     window.open(
